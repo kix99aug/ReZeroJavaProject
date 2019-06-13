a1fb82517832fb5b4e86e4af516e4d1451ba59c9
Kix <kix99aug@gmail.com>
2019-06-13 21:16:54 +0800

    Merged

--- MenuPanel.java
+++ MenuPanel.java
@@ -35,12 +35,6 @@ public class MenuPanel extends JPanel implements ActionListener {
   MenuPanel(MainFrame mf){
     this.mainframe = mf;
     this.setSize(this.mainframe.getSize());
-<<<<<<< HEAD
-    this.setLayout(null);
-    startButton.setBounds(1280/2-buttonW/2, 400, buttonW, buttonH);
-=======
-
-    this.setLayout(null);
     this.add(logo);
     logo.setBounds(490,40,300,250);
     logo.setBorder(null);
@@ -48,7 +42,6 @@ public class MenuPanel extends JPanel implements ActionListener {
     this.setLayout(null);
 
     startButton.setBounds(1280/2-buttonW/2, 300, buttonW, buttonH);
->>>>>>> 9d1962fa8b68223444d1a5adddc474ad80ecc5c9
     startButton.setIcon(startImage[0]);
     startButton.setRolloverIcon(startImage[1]);
     startButton.setPressedIcon(startImage[2]);
@@ -56,12 +49,6 @@ public class MenuPanel extends JPanel implements ActionListener {
     startButton.setContentAreaFilled(false);
     startButton.addActionListener(this);
     this.add(startButton);
-<<<<<<< HEAD
-  }
-  public void actionPerformed(ActionEvent e) {
-    if (e.getSource() == startButton) {
-      //super.
-=======
 
     bagButton.setBounds(1280/2-buttonW/2, 400, buttonW, buttonH);
     bagButton.setIcon(bagImage[0]);
@@ -85,24 +72,16 @@ public class MenuPanel extends JPanel implements ActionListener {
   public void actionPerformed(ActionEvent e) {
     if (e.getSource() == startButton) {
       startButton.setLocation(startButton.getX()-1, startButton.getY()-1);
->>>>>>> 9d1962fa8b68223444d1a5adddc474ad80ecc5c9
     }
     else if(e.getSource() == exitButton){
       System.exit(0);
     }
-<<<<<<< HEAD
-=======
     /*else if(e.getSource() == bagButton){
      // mf.getContentPane().add();
     }*/
->>>>>>> 9d1962fa8b68223444d1a5adddc474ad80ecc5c9
 } 
   public void paintComponent(Graphics g){
     super.paintComponent(g);
     g.drawImage(bgImage, 0, 0,this.mainframe.getWidth(),this.mainframe.getHeight(),mainframe);
   }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 9d1962fa8b68223444d1a5adddc474ad80ecc5c9
