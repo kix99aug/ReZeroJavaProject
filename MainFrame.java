d7e856e94e4b7966add717901ac6aa80f45216b3
Kix <kix99aug@gmail.com>
2019-06-13 20:30:52 +0800

    OK

--- MainFrame.java
+++ MainFrame.java
@@ -1,15 +1,16 @@
 import java.awt.*;
+import java.util.HashMap;
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
-  JPanel menu = null;
+  HashMap<String,JPanel> panels = new HashMap<String,JPanel>();
   MainFrame(){
     super("Game");
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setSize(1280, 720);
-    menu = new MenuPanel(this);
-    menu.setVisible(true);
-    this.getContentPane().add(menu);
+    panels.put("menu", new MenuPanel(this));
+    panels.get("menu").setVisible(true);
+    this.getContentPane().add(panels.get("menu"));
     this.setVisible(true);
   }
 }
