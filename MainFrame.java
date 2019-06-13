8573dbc04009f5af131b78c0eaf877d2e1859b67
aa254412 <aa254412@gmail.com>
2019-06-13 17:51:40 +0800

    test

--- MainFrame.java
+++ MainFrame.java
@@ -1,10 +1,19 @@
 import java.awt.*;
+import java.awt.event.*;
+import java.util.HashSet;
+import java.util.Set;
 import javax.swing.*;
+import javax.swing.event.*;
 
 public class MainFrame extends JFrame {
   JPanel menu = null;
+  
+  Image cursorImage = new ImageIcon("./img/cursor.png").getImage();
+  Point hotspot = new Point(0, 0);
+  String cursorName = "Lightsaber Cursor";
   MainFrame(){
     super("Game");
+    this.setCursor(getToolkit().createCustomCursor(cursorImage, hotspot, cursorName));
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setSize(1280, 720);
     menu = new MenuPanel(this);
--- MenuPanel.java
+++ MenuPanel.java
@@ -4,11 +4,19 @@ import javax.swing.*;
 
 public class MenuPanel extends JPanel implements ActionListener {
   MainFrame mainframe = null;
+
+  Image bgImage = new ImageIcon("./img/bg.png").getImage(); 
+  ImageIcon logoImage = new ImageIcon("./img/logo.png");
+
   JButton startButton = new JButton();
   JButton bagButton = new JButton();
   JButton exitButton = new JButton();
+
+  JLabel logo=new JLabel();
+
   int buttonW = 190,buttonH = 80;
-  Image bgImage = new ImageIcon("./img/bg.png").getImage(); 
+
+
   ImageIcon startImage[] = {
     new ImageIcon(new ImageIcon("./img/btn/start.png").getImage().getScaledInstance(buttonW, buttonH, Image.SCALE_DEFAULT)),
     new ImageIcon(new ImageIcon("./img/btn/start_r.png").getImage().getScaledInstance(buttonW, buttonH, Image.SCALE_DEFAULT)),
@@ -27,8 +35,15 @@ public class MenuPanel extends JPanel implements ActionListener {
   MenuPanel(MainFrame mf){
     this.mainframe = mf;
     this.setSize(this.mainframe.getSize());
+
     this.setLayout(null);
-    startButton.setBounds(1280/2-buttonW/2, 400, buttonW, buttonH);
+    this.add(logo);
+    logo.setBounds(1280/2-buttonW/2, 100,450,100);
+    logo.setBorder(null);
+    logo.setIcon(logoImage);
+    this.setLayout(null);
+
+    startButton.setBounds(1280/2-buttonW/2, 300, buttonW, buttonH);
     startButton.setIcon(startImage[0]);
     startButton.setRolloverIcon(startImage[1]);
     startButton.setPressedIcon(startImage[2]);
@@ -36,6 +51,25 @@ public class MenuPanel extends JPanel implements ActionListener {
     startButton.setContentAreaFilled(false);
     startButton.addActionListener(this);
     this.add(startButton);
+
+    bagButton.setBounds(1280/2-buttonW/2, 400, buttonW, buttonH);
+    bagButton.setIcon(bagImage[0]);
+    bagButton.setRolloverIcon(bagImage[1]);
+    bagButton.setPressedIcon(bagImage[2]);
+    bagButton.setBorder(null);
+    bagButton.setContentAreaFilled(false);
+    bagButton.addActionListener(this);
+    this.add(bagButton);
+
+    exitButton.setBounds(1280/2-buttonW/2, 500, buttonW, buttonH);
+    exitButton.setIcon(exitImage[0]);
+    exitButton.setRolloverIcon(exitImage[1]);
+    exitButton.setPressedIcon(exitImage[2]);
+    exitButton.setBorder(null);
+    exitButton.setContentAreaFilled(false);
+    exitButton.addActionListener(this);
+    this.add(exitButton);
+    
   }
   public void actionPerformed(ActionEvent e) {
     if (e.getSource() == startButton) {
@@ -44,6 +78,9 @@ public class MenuPanel extends JPanel implements ActionListener {
     else if(e.getSource() == exitButton){
       System.exit(0);
     }
+    /*else if(e.getSource() == bagButton){
+     // mf.getContentPane().add();
+    }*/
 } 
   public void paintComponent(Graphics g){
     super.paintComponent(g);
