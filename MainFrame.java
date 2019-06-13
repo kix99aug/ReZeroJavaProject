52df51df87bccf854d26aad21fb2e8790aec4556
unknown <as463431@gmail.com>
2019-06-13 23:43:10 +0800

    fk

--- MainFrame.java
+++ MainFrame.java
@@ -15,10 +15,14 @@ public class MainFrame extends JFrame {
     this.setSize(1280, 720);
     panels.put("inputname", new InputNamePanel(this));
     panels.put("menu", new MenuPanel(this));
-    panels.get("inputname").setVisible(true);
-    panels.get("menu").setVisible(false);
+    panels.put("bag", new BagPanel(this));
+    panels.get("inputname").setVisible(false);
+    panels.get("menu").setVisible(true);
+    panels.get("bag").setVisible(false);
+    
     this.getContentPane().add(panels.get("inputname"));
     this.getContentPane().add(panels.get("menu"));
+    this.getContentPane().add(panels.get("bag"));
     this.setVisible(true);
   }
 }
--- MenuPanel.java
+++ MenuPanel.java
@@ -35,10 +35,6 @@ public class MenuPanel extends JPanel implements ActionListener {
   MenuPanel(MainFrame mf){
     this.mainframe = mf;
     this.setSize(this.mainframe.getSize());
-<<<<<<< HEAD
-    this.setLayout(null);
-    startButton.setBounds(1280/2-buttonW/2, 400, buttonW, buttonH);
-=======
 
     this.setLayout(null);
     this.add(logo);
@@ -48,7 +44,6 @@ public class MenuPanel extends JPanel implements ActionListener {
     this.setLayout(null);
 
     startButton.setBounds(1280/2-buttonW/2, 300, buttonW, buttonH);
->>>>>>> 9d1962fa8b68223444d1a5adddc474ad80ecc5c9
     startButton.setIcon(startImage[0]);
     startButton.setRolloverIcon(startImage[1]);
     startButton.setPressedIcon(startImage[2]);
@@ -56,12 +51,6 @@ public class MenuPanel extends JPanel implements ActionListener {
     startButton.setContentAreaFilled(false);
     startButton.addActionListener(this);
     this.add(startButton);
-<<<<<<< HEAD
-  }
-  public void actionPerformed(ActionEvent e) {
-    if (e.getSource() == startButton) {
-      //super.
-=======
 
     bagButton.setBounds(1280/2-buttonW/2, 400, buttonW, buttonH);
     bagButton.setIcon(bagImage[0]);
@@ -85,24 +74,20 @@ public class MenuPanel extends JPanel implements ActionListener {
   public void actionPerformed(ActionEvent e) {
     if (e.getSource() == startButton) {
       startButton.setLocation(startButton.getX()-1, startButton.getY()-1);
->>>>>>> 9d1962fa8b68223444d1a5adddc474ad80ecc5c9
     }
     else if(e.getSource() == exitButton){
       System.exit(0);
     }
-<<<<<<< HEAD
-=======
+    else if(e.getSource() == bagButton){
+      this.setVisible(false);
+      this.mainframe.panels.get("bag").setVisible(true);
+    }
     /*else if(e.getSource() == bagButton){
      // mf.getContentPane().add();
     }*/
->>>>>>> 9d1962fa8b68223444d1a5adddc474ad80ecc5c9
 } 
   public void paintComponent(Graphics g){
     super.paintComponent(g);
     g.drawImage(bgImage, 0, 0,this.mainframe.getWidth(),this.mainframe.getHeight(),mainframe);
   }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 9d1962fa8b68223444d1a5adddc474ad80ecc5c9
