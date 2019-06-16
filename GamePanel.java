24b6b1dc43e5943b2f3d68d49c1d39c32ba81d40
Arpan <sheep5168947@gmail.com>
2019-06-16 16:26:23 +0800

    gamepart obstacle

--- GamePanel.java
+++ GamePanel.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.Random;
 
 public class GamePanel extends AbstractPanel {
   MainFrame mainframe = null;
@@ -8,16 +9,15 @@ public class GamePanel extends AbstractPanel {
   Image skyImage = new ImageIcon("./img/sky1.png").getImage(); 
   Image groundImage = new ImageIcon("./img/ground1.png").getImage(); 
 
-  ImageIcon obstacle[] = {
-    new ImageIcon(new ImageIcon("./img/MapSource/圖層7.png").getImage().getScaledInstance( 107,  85, Image.SCALE_DEFAULT)),
-    new ImageIcon(new ImageIcon("./img/MapSource/圖層8.png").getImage().getScaledInstance( 107,  85, Image.SCALE_DEFAULT)),
-    new ImageIcon(new ImageIcon("./img/MapSource/圖層9.png").getImage().getScaledInstance( 107,  85, Image.SCALE_DEFAULT)),
-    new ImageIcon(new ImageIcon("./img/MapSource/圖層10.png").getImage().getScaledInstance( 107,  85, Image.SCALE_DEFAULT)),
-    new ImageIcon(new ImageIcon("./img/MapSource/圖層11.png").getImage().getScaledInstance( 107,  85, Image.SCALE_DEFAULT)),
-    new ImageIcon(new ImageIcon("./img/MapSource/圖層12.png").getImage().getScaledInstance( 107,  85, Image.SCALE_DEFAULT)),
+  Image obstacle[] = {
+    new ImageIcon("./img/MapSource/obstacle2.png").getImage(),
+    new ImageIcon("./img/MapSource/obstacle3.png").getImage(),
+    new ImageIcon("./img/MapSource/obstacle4.png").getImage(),
+    new ImageIcon("./img/MapSource/obstacle5.png").getImage(),
+    new ImageIcon("./img/MapSource/obstacle6.png").getImage()
   }; 
   int bgx = 0,bgy=0;
-
+  Random ran = new Random();
   GamePanel(MainFrame mf){
     this.mainframe = mf;
     this.setSize(this.mainframe.getSize());
@@ -31,6 +31,9 @@ public class GamePanel extends AbstractPanel {
     for(int i = 0;i<1280;i+=319){
       g.drawImage(skyImage, i, 0,mainframe);
       g.drawImage(groundImage, i, 320,mainframe);
+    }
+    for(int i = 0; i < 5;i++){
+      g.drawImage(obstacle[ran.nextInt(5)], ran.nextInt(1280), ran.nextInt(80)+i*80+320-214,175,214,mainframe);
     }
   }
 }
