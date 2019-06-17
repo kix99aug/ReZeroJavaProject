3a7709de9d06fd2191efd8649a90e065a6a977d2
Arpan <sheep5168947@gmail.com>
2019-06-17 16:51:52 +0800

    gamepanel掛掉

--- GamePanel.java
+++ GamePanel.java
@@ -18,22 +18,46 @@ public class GamePanel extends AbstractPanel {
   }; 
   int bgx = 0,bgy=0;
   Random ran = new Random();
+ 
+  int[] store_X;
+  int[] store_Y;
+  int[] choose;
   GamePanel(MainFrame mf){
     this.mainframe = mf;
     this.setSize(this.mainframe.getSize());
     this.setLayout(null);
-  }
+    for(int i =0; i <8; i++){
+      store_X[i] = ran.nextInt(2560);
+      store_Y[i] = ran.nextInt(50);
+      choose[i] = ran.nextInt(5);
+    }
+    new Thread(){
+		  public void run(){
+			  while(true){
+				  repaint();
+				  try {
+				  	Thread.sleep(10);
+          }
+          catch (InterruptedException e) {
+			  		e.printStackTrace();
+			  	}
+			  } 
+		  }
+    }.start();
+    
+}
   public void actionPerformed(ActionEvent e) {
     
-  } 
+  }
   public void paint(Graphics g){
+    
     super.paint(g);
     for(int i = 0;i<2560;i+=319){
       g.drawImage(skyImage, i, 0,mainframe);
       g.drawImage(groundImage, i, 320,mainframe);
     }
     for(int i = 0; i < 8;i++){
-      g.drawImage(obstacle[ran.nextInt(5)], ran.nextInt(2560), ran.nextInt(50)+i*50+320-214,175,214,mainframe);
+      g.drawImage(obstacle[choose[i]], store_X[i], store_Y[i]+i*50+320-214,175,214,mainframe);
     }
   }
 }
